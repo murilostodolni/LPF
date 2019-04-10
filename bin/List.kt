38a30cfa959160be48d5c22b0168a5d5418e43bd
Murilo Stodolni <m_campanhol@hotmail.com>
2019-04-10 20:53:49 -0300

    Adicionando metodo comprimir a lista

--- bin/List.kt
+++ bin/List.kt
@@ -32,20 +32,34 @@ fun reverso(l: List): List = when(l){
 //palindromo
 fun palindromo(l: List):Boolean = l == reverso(l)
 
+//comprimir
+fun comprimir(l: List): List = when(l){
+	is Node -> Node(l.info, auxComprimir(l.prox, l.info))
+	else -> Nil
+}
+
+fun auxComprimir(l1: List, i: Int): List = when{
+	l1 is Node && l1.info == i -> auxComprimir(l1.prox, i)
+	l1 is Node && l1.info != i -> Node(l1.info, auxComprimir(l1.prox, l1.info))
+	else -> Nil
+}
+
 fun main(args: Array<String>) {
-	val lista = Node(1, Node(2, Node(3, Node(4, Nil))))
-	val lista1 = Node(5, Node(6, Node(5, Nil)))
+	val lista = Node(1, Node(2, Node(3, Nil)))
+	val lista1 = Node(5, Node(5, Node(6, Node(6, Node(7, Nil)))))
  
 	println(concatenar(lista, lista1))
  
 	println(tamanho(lista))
 	println(tamanho(lista1))
  
-	println("Tamnho da Lista: ${get(lista, 3)}")
+	println(get(lista, 3))
  
 	println(reverso(lista))
  
-	println(palindromo(lista))
+	println(palindromo(lista1))
+ 
+	println(comprimir(lista1))
  
 	
 }
